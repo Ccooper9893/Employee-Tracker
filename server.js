9258d0146142b0e510713f71311f8c449eed578d
Cody Cooper <cody_codster@live.com>
2023-01-06 17:04:56 -0600

    Created server.js file and imported required npm libraries into file

---
+++ server.js
@@ -0,0 +1,3 @@
+const mysql = require('mysql2'); //Imports mysql2 library so we can interact with the mySQL databases;
+const inquirer = require('inquirer'); //Imports inquirer library so we can use CLI
+const cTable = require('console.table');// Use console.table similiar to console.log. Makes it beautiful!
