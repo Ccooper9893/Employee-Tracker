d429aedd26b3150f15b1e46831f4b1bf740d60d6
Cody Cooper <cody_codster@live.com>
2023-01-09 16:05:20 -0600

    Updated mysql2 queries with new classes

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const mysql = require('mysql2'); //Imports mysql2 library so we can interact wit
 const inquirer = require('inquirer'); //Imports inquirer library so we can use CLI
 const cTable = require('console.table');// Use console.table similiar to console.log. Makes it beautiful!
 
+//Importing classes for role, department, and employee
+const Department = require('./lib/department');
+const Employee = require('./lib/employee');
+const Role = require('./lib/role');
+
+//Creating empty arrays to store row objects to display in inquirer prompts
 let departments = [];
 let employees = [];
 let roles = [];
 
-// Connect to the database
+//Connect to the MySQL database
 const db = mysql.createConnection(
       {
           host: 'localhost',
@@ -18,17 +24,15 @@ const db = mysql.createConnection(
       // Add functions for inquirer questions
   );
 
-// Array of CMS options
-const selectionArr = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
-
+//Query company database to save tables with information to an related arrays
 db.query('SELECT * FROM employee;', (err, row) => {
     if (err) {
         console.log(err)
     }
     for (emp of row) {
-        employees.push(emp)
+        let newEmp = new Employee(emp.id, emp.first_name, emp.last_name, emp.role_id, emp.manager_id)
+        employees.push(newEmp);
     }
-    console.log(employees)
 });
 
 db.query('SELECT * FROM role;', (err, row) => {
@@ -36,7 +40,8 @@ db.query('SELECT * FROM role;', (err, row) => {
         console.log(err)
     }
     for (role of row) {
-        roles.push(role)
+        let newRole = new Role(role.title, role.salary, role.department_id)
+        roles.push(newRole)
     }
     console.log(roles)
 });
@@ -46,12 +51,21 @@ db.query('SELECT * FROM department;', (err, row) => {
         console.log(err)
     }
     for (dep of row) {
-        departments.push(dep)
+        let newDep = new Department(dep.id, dep.name)
+        departments.push(newDep)
     }
-    console.log(departments)
-    console.log(departments[0].name);
 });
 
+// Array of CMS options
+const selectionArr = [
+    'View All Employees', 
+    'Add Employee', 
+    'Update Employee Role', 
+    'View All Roles', 
+    'Add Role', 
+    'View All Departments', 
+    'Add Department'
+];
 
 /* Bonus
     - Update Employee Managers
