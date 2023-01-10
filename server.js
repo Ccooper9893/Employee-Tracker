c4ce5a03ef8701507bc0adec66d6e4cef9915bf4
Cody Cooper <cody_codster@live.com>
2023-01-10 11:00:00 -0600

    Debugging database query functions

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const Role = require('./lib/role');
 let departments = [];
 let employees = [];
 let roles = [];
+let managers = [];
 
 //Connect to the MySQL database
 
@@ -24,16 +25,21 @@ const db = mysql.createConnection(
   );
 
 // Query company database to save tables with information to an related arrays
+
+function queryEmployees() {
 db.promise().query('SELECT * FROM employee;')
       .then(([rows, fields]) => {
         for (emp of rows) {
             let newEmp = new Employee(emp.id, emp.first_name, emp.last_name, emp.role_id, emp.manager_id)
             employees.push(newEmp);
+            if (emp.manager_id == null) {
+                managers.push(newEmp)
+            }
         }
       })
       .catch((err) => {console.log('There has been an query error', err)});
-    
-
+    }
+function queryRoles() {
 db.promise().query('SELECT * FROM role;')
     .then(([rows, fields]) => {
         for (role of rows) {
@@ -42,9 +48,9 @@ db.promise().query('SELECT * FROM role;')
         }
     })
     .catch((err) => {console.log('There has been an query error', err)});    
+}
 
-
-
+function queryDepartments() {
 db.promise().query('SELECT * FROM department;')
     .then(([rows, fields]) => {
         for (dep of rows) {
@@ -53,13 +59,14 @@ db.promise().query('SELECT * FROM department;')
         }
     })
     .catch((err) => {console.log('There has been an query error', err)});
-
+}
 
 // Array of CMS options
 const selectionArr = [
     'View All Employees', 
     'Add Employee', 
-    'Update Employee Role', 
+    'Update Employee Role',
+    'Update Employee Manager', 
     'View All Roles', 
     'Add Role', 
     'View All Departments', 
@@ -69,6 +76,12 @@ const selectionArr = [
 
 //Questions for add Employee option
 function start() {
+
+    queryDepartments();
+    queryEmployees();
+    queryRoles();
+
+
     //Creating empty arrays to store row objects to display in inquirer prompts
     inquirer.prompt(
         {
@@ -93,6 +106,8 @@ function start() {
             case 'View All Departments': viewAllDepartments();
             break;
             case 'Add Department': addDepartment();
+            break;
+            case 'Update Employee Manager': updateManager();
             break;
             case 'Quit': db.end();
             break;
@@ -154,7 +169,7 @@ function viewAllRoles() {
 }
 
 function addEmployee() {
-    inquirer.prompt(addEmployeeQuestions)
+    inquirer.prompt(addEmployeeQ)
     .then((data) => {
 
         let mId;
@@ -176,18 +191,18 @@ function addEmployee() {
             
             })
         .catch(err => console.log(err));
-        db.promise().query(`SELECT id FROM employee WHERE first_name = ?`, [data.empFirstName])
-        .then(([rows, fields]) => {
-            let newEmp = new Employee(rows.id, data.empFirstName, data.empLastName, rId, mId)
-            employees.push(newEmp);
-        })
+        // db.promise().query(`SELECT id FROM employee WHERE first_name = ?`, [data.empFirstName])
+        // .then(([rows, fields]) => {
+        //     let newEmp = new Employee(rows.id, data.empFirstName, data.empLastName, rId, mId)
+        //     employees.push(newEmp);
+        // })
         start()
     })
     .catch((err) => {console.log('There has been an query error', err)})
 };
 
 function updateEmployee() {
-    inquirer.prompt(updateRoleQuestions)
+    inquirer.prompt(updateRoleQ)
     .then(data => {
         let rId = roles[roles.map(index => index.title).indexOf(data.roleChoice)].id
         let empIndex = employees.map(index => index.first_name).indexOf(data.empChoice);
@@ -204,16 +219,62 @@ function updateEmployee() {
 function addDepartment() {
     inquirer.prompt(addDepartmentQ)
     .then((data) => {
-        db.promise().query(`INSERT INTO department VALUES ?`, [data.newDep])
-        .then(data => console.log('\nDepartment has been added.'))
+        
+        db.promise().query(`INSERT INTO department (name) VALUES (?)`, [data.newDep])
+        .then((data) => {
+            console.log('\nDepartment has been added.')
+        })
         .catch(err => console.log('There has been an query error', err))
 
+        // db.promise().query(`SELECT id FROM department WHERE name = ?`, [data.newDep])
+        //     .then((row) => {
+        //         console.log(row)
+        //         // let dep = new Department(row[0].id, data.newDep)
+        //         // departments.push(dep)
+        //     })
+        //     .catch(err => console.log('There has been an query error', err))
+
+        
+        start()
     })
     .catch(err => console.log('There has been an error!', err))
 }
 
+function addRole() {
+    inquirer.prompt(addRoleQ)
+    .then((data) => {
+        //Grabs the department id of selected department
+        let dId = departments[departments.map(e => e.name).indexOf(data.desDep)].id;
+        
+        db.promise().query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [data.newRole, data.newSal, dId])
+            .then(data => console.log('Role has been added.'))
+            .catch(err => console.log('There has been an query error', err))
+                
+        start();
+    })
+    .catch(err => console.log('There has been an error!', err))
+}
+
+function updateManager() {
+    inquirer.prompt(updateManagerQ)
+    .then(data => {
+
+        //Grabs Ids for both the employee for update and manager replacing
+        let eId = employees.map(index => index.first_name).indexOf(data.emp)
+        let mId = employees[employees.map(index => index.first_name).indexOf(data.man)].id
+
+        db.promise().query(`UPDATE employee SET manager_id = ? WHERE first_name = ?`, [mId, data.emp])
+        .then(data => console.log('You have changed the employee\'s manager.'))
+        .catch(err => console.log('There has been an query error', err))
+
+        employees[eId].manager_id = mId;
+        start()
+    })  
+    .catch(err => console.log('There has been an error!', err))
+}
+
 /*----------------------Inquirer Questions-----------------------*/ 
-const addEmployeeQuestions = [
+const addEmployeeQ = [
     {
         type: 'input',
         name: 'empFirstName',
@@ -254,7 +315,7 @@ const addEmployeeQuestions = [
     }
 ];
 
-const updateRoleQuestions = [
+const updateRoleQ = [
     {
     type: 'list',
     name: 'empChoice',
@@ -266,8 +327,9 @@ const updateRoleQuestions = [
     {
         type: 'list',
         name: 'roleChoice',
+        message: 'What is the employee\'s new role?',
         choices: () => {
-            return roles.map(index => index.title)
+            return roles.map(index => index.title);
         }
     }
 ]
@@ -277,12 +339,58 @@ const addDepartmentQ = [
         type: 'input',
         name: 'newDep',
         message: 'Please enter the department you would like to add.',
-        validate: userInput => {
-            userInput ? true : console.log('\nPlease enter a valid department name.')
+        vaidate: userInput => {
+            return isNaN(userInput) ? true : console.log('Please enter a valid department.')
         }
     }
 ]
 
+const addRoleQ = [
+    {
+        type: 'input',
+        name: 'newRole',
+        message: 'Please enter a role you would like to add.',
+        validate: userInput => {
+            return userInput ? true : console.log('Please enter a valid role!')
+        }
+    },
+    {
+        type: 'input',
+        name: 'newSal',
+        message: 'Please enter the salary of this postion in dollars. Ex. 150000 for $150,000 a year.',
+        validate: userInput => {
+           return !isNaN(userInput) ? true : console.log('Please enter a number. Do not include commas!')
+        }
+    },
+    {
+        type: 'list',
+        name: 'desDep',
+        message: 'Please choose which department this role should be designated.',
+        choices: () => {
+            return departments.map(index => index.name)
+        }
+        
+    }
+];
+
+const updateManagerQ = [
+    {
+        type: 'list',
+        name: 'emp',
+        message: 'Please choose an employee whose manager you want to change.',
+        choices: () => {
+            return employees.map(index => index.first_name); 
+            }
+    },
+    {
+        type: 'list',
+        name: 'man',
+        message: 'Please choose the manager that you would like assign to the employee.',
+        choices: () => {
+            return managers.map(index => index.first_name)
+            }
+    }
+];
 start();
 
 /* Bonus
